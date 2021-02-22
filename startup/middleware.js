ff27dc646de8867e7f39de37d57ce15e937f8bbf
Cris Fandino <sicrnujnuj@gmail.com>
2021-02-22 16:50:38 +0800

    remove secure cookie

--- startup/middleware.js
+++ startup/middleware.js
@@ -12,7 +12,6 @@ module.exports = function (app) {
   )
   app.use(
     session({
-      secure: process.env.NODE_ENV === 'production',
       name: '__user',
       secret: config.cookieSecret,
       maxAge: 60 * 60 * 24 * 1000 * Number(config.cookieAge), // 15 days
